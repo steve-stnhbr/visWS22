39c01547e983005bfdbc3a6d106a12a5ed8b74c3
waldner <waldner@cg.tuwien.ac.at>
2021-09-17 15:40:21 +0200

    Shaders are now in external essl files. Due to async loading from files, shader handling is now a little bit more tricky (requires async / await).

--- js/cubeShader.js
+++ js/cubeShader.js
@@ -1,38 +1,8 @@
-class CubeShader {
+class CubeShader extends Shader{
     constructor(side, scale) {
-        this.material = new THREE.ShaderMaterial
-        ({
-            uniforms: {
-                scale: new THREE.Uniform( new THREE.Vector3(scale[0], scale[1], scale[2]) )
-            },
-            vertexShader: this.vertexShader(),
-            fragmentShader: this.fragmentShader(),
-            transparent: true,
-            side: side
-        });
-    }
-
-    vertexShader(){
-        return `
-        varying vec3 vPosition; 
-        uniform vec3 scale; 
-        
-        void main() {
-            vPosition = position + vec3(0.5, 0.5, 0.5); 
-            vec4 mvPosition = modelViewMatrix * vec4( position * scale, 1.0);
-            gl_Position = projectionMatrix * mvPosition;
-        }
-        `;
-    }
-
-    fragmentShader(){
-        return `
-        varying vec3 vPosition;
-        
-        void main(){
-            vec3 uvw = vec3(vPosition.x, vPosition.y ,vPosition.z);
-            gl_FragColor = vec4(uvw, 1.0); 
-        }
-        `;
+        super("cube_vert", "cube_frag");
+        this.setUniform("scale", new THREE.Vector3(scale[0], scale[1], scale[2]));
+        this.material.side = side;
     }
 }
+
